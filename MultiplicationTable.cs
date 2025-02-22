fce284b7400176f3a56e9129f51f958be9c280c5
Puja Borse <107254941+Pujaborse13@users.noreply.github.com>
2025-02-22 15:31:30 +0530

    Add files via upload

---
+++ MultiplicationTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewSession1_Puja
+{
+    internal class MultiplicationTable
+    {
+
+        public void MultiplicationTableofGivenNum()
+        {
+            Console.Write("Enter the Number :");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+
+            }
+        }
+    }
+}
+
---
+++ TypeConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewSession1_Puja
+{
+    internal class TypeConversion
+    {
+        public void TypeConversionImplicitAndExplicit()
+        {
+
+            //implicit type Conversion
+            int num = 10;
+            double toDouble = num;
+            Console.WriteLine("Implicit Conversion int to double: " + toDouble);
+
+
+            //Explicit Type Conversion
+            double num2 = 9.99;
+            int toInt = (int)num2; 
+            Console.WriteLine("Explicit Conversion (double to int): " + toInt);
+
+        }
+
+    }
+}
