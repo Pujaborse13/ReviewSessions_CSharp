24f96d345dd722b12b6cc42a08e805a73f2c318c
Puja Borse <107254941+Pujaborse13@users.noreply.github.com>
2025-02-22 15:33:58 +0530

    ReviewSession_2

---
+++ Employee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewSession_2
+{
+     class Employee
+     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        
+        public Employee(int id, string name, string department)   
+        {
+            Id = id;
+            Name = name;
+            Department = department;
+        }
+
+        public void Display()  
+        {
+            Console.WriteLine($"ID: {Id}, Name: {Name}, Department: {Department}");
+        }
+     }
+
+    class EmployeeData
+    {       //List To Store Employee Data.
+            private List<Employee> employees = new List<Employee>();
+
+            //to Add Employee
+            public void AddEmployee(Employee emp) {
+                employees.Add(emp);
+          
+            }
+
+          //To Remove Employee
+            public void RemoveEmployee(int id)
+            {
+            Employee emp = employees.Find(e => e.Id == id);
+
+            if (emp != null){
+                employees.Remove(emp);
+                Console.WriteLine($"Employee with ID {id} removed.");
+            }
+
+            else{
+                Console.WriteLine("Employee Not Found");
+             }
+           }
+
+
+        //Search Employee
+       public Employee SearchById(int id)
+       {
+            Console.WriteLine("Employee found by given ID");
+            return employees.Find(e => e.Id == id);
+       }
+
+        //Display Employee
+        public void DisplayEmp()
+        {
+                if (employees.Count == 0){
+                    Console.WriteLine("No Employee Found");
+                    return;
+
+                }
+
+                Console.WriteLine("Employee List :");
+            foreach (Employee emp in employees){
+                emp.Display();
+
+            }
+
+        }
+    }
+
+}
---
+++ FindMax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewSession_2
+{
+    internal class FindMax{
+        public static T FindMaxNum<T>(T[] array) where T : IComparable<T>{ 
+            T max = array[0];
+
+            foreach (T item in array) {
+                if (item.CompareTo(max) > 0)   
+                    max = item;
+            }
+
+            return max;
+
+        }
+
+        public void Input()
+        {
+            int[] intArray = { 1, 5, 3, 9, 2 };
+            double[] doubleArray = { 2.5, 7.8, 1.2, 6.4 };
+            string[] stringArray = { "hiee", "puja", "Borse" };
+
+            Console.WriteLine("Max Int: " + FindMaxNum(intArray));
+            Console.WriteLine("Max Double: " + FindMaxNum(doubleArray));
+            Console.WriteLine("Max String: " + FindMaxNum(stringArray));
+
+
+        }
+
+
+    }
+}
