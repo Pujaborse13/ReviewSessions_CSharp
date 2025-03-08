fec72c8246ca3610999e3e6fc91135968f942110
Puja Borse <107254941+Pujaborse13@users.noreply.github.com>
2025-03-08 12:48:34 +0530

    ReviewSession_5

---
+++ CheckAge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewSession_5
+{
+    internal class CheckAge
+    {
+        //Create a custom exception InvalidAgeException that is thrown when a user enters an age less than 18.
+
+        public void ValidateAge()
+        {
+            try
+            {
+                Console.WriteLine("Enter your age");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age < 18)
+                {
+                    throw new InvalidAgeException("Age Must be More than 18");
+
+                }
+
+                Console.WriteLine("Valid age");
+            }
+
+
+            catch (InvalidAgeException ex)
+            {
+                Console.WriteLine("Error" + ex.Message);
+
+            }
+
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Please enter a valid number.");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ StackDemo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewSession_5
+{
+  public class Node<T>
+    {
+        public T Data;
+        public Node<T> Next;
+
+        public Node(T Data)
+        {
+            this.Data = Data;
+            this.Next = null;
+        
+        }
+
+
+    }
+
+    class StackDemo<T>
+    { 
+    
+        private Node<T> top;
+
+        public StackDemo()
+        {
+
+            top = null;
+        }
+
+        public void Push(T Value)
+        {
+            Node<T> newNode = new Node<T>(Value);
+            newNode.Next = top;
+            top = newNode;
+            Console.WriteLine($"Pushed : {Value} into stack");
+        }
+
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack UnderFlow! No elements to pop");
+                return default;
+            }
+            T poppedValue = top.Data;
+            top = top.Next;
+            Console.WriteLine($"Popped :  {poppedValue} from stack");
+            return poppedValue;
+
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty! No elements to peek.");
+                return default;
+            }
+            return top.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+
+            }
+
+            Console.Write("Stack Elements : ");
+            Node<T> temp = top;
+            while (temp != null)
+            {
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+
+        }
+
+
+    }
+
+
+
+}
